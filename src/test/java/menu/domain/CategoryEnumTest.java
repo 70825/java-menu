85deb354d4e59b042afb213e05604cc4a60538d2
Dabeen Jeong <hello70825@gmail.com>
2022-12-17 16:43:53 +0900

    test: CategoryEnum 테스트 추가

---
+++ src/test/java/menu/domain/CategoryEnumTest.java
@@ -0,0 +1,37 @@
+package menu.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CategoryEnumTest {
+
+    @DisplayName("각 값마다 올바른 메뉴 이름을 반환하는지 확인한다.")
+    @MethodSource("createDate")
+    @ParameterizedTest
+    void checkTransferCategory(Integer testValue, String expected) {
+        // given
+        String actual;
+
+        // when
+        actual = CategoryEnum.getCategoryName(testValue);
+
+        // then
+        assertEquals(actual, expected, "[ERROR] 예상한 카테고리 이름과 다릅니다.");
+    }
+
+    static Stream<Arguments> createDate() {
+        return Stream.of(
+                Arguments.of(1, "일식"),
+                Arguments.of(2, "한식"),
+                Arguments.of(3, "중식"),
+                Arguments.of(4, "아시안"),
+                Arguments.of(5, "양식")
+        );
+    }
+}
