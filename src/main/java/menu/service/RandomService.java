f23010478256e91bd552f0dbc0600a4f0312f5aa
Dabeen Jeong <hello70825@gmail.com>
2022-12-17 14:59:36 +0900

    feat: 특정 카테고리에서 랜덤으로 메뉴를 선택하는 기능 추가

--- src/main/java/menu/service/RandomService.java
+++ src/main/java/menu/service/RandomService.java
@@ -1,10 +1,14 @@
 package menu.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import menu.controller.MainController;
+import menu.domain.Categories;
 import menu.domain.Category;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomService {
 
@@ -39,5 +43,22 @@ public class RandomService {
             return "양식";
         }
         throw new IllegalArgumentException("[ERROR]");
+    }
+
+    public List<String> randomGetMenu(String category, List<String> names) {
+        List<String> result = new ArrayList<>();
+        List<String> categoryMenu = Categories.getCategory(category);
+
+        names.forEach(name -> {
+            List<String> hate = MainController.people.get(name);
+            List<String> canEatFood = categoryMenu.stream()
+                    .filter(menu -> !hate.contains(menu))
+                    .collect(Collectors.toList());
+            String resultRandom = Randoms.shuffle(canEatFood).get(0);
+            result.add(resultRandom);
+            MainController.people.get(name).add(resultRandom);
+        });
+
+        return result;
     }
 }
