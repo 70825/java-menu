fba539a91e295dc18f9ba9c68de2d0815978d304
Dabeen Jeong <hello70825@gmail.com>
2022-12-17 14:28:48 +0900

    feat: 못 먹는 메뉴 입력 적용

--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -1,25 +1,37 @@
 package menu.controller;
 
+import menu.domain.Categories;
 import menu.service.InitService;
 import menu.view.MainView;
+import menu.view.NotEatView;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MainController {
     private static final String NEWLINE = "\n";
     InitService initService = new InitService();
+    HashMap<String, List<String>> people = new HashMap<>();
 
     public void start() {
         initService.init();
 
         while (true) {
             try {
+                people.clear();
                 MainView.printMainScreen();
                 List<String> names = MainView.getMainValue();
-                System.out.println(names);
+                startNotEat(names);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage() + NEWLINE);
             }
         }
+    }
+
+    private void startNotEat(List<String> names) {
+        names.forEach(name -> {
+            people.put(name, List.of());
+            people.put(name, NotEatView.getNotEatValue(name));
+        });
     }
 }
