bcd1f73dfd15b8c945e96b9c92e5fec05eee2d8f
Dabeen Jeong <hello70825@gmail.com>
2022-12-17 16:54:47 +0900

    test: 카테고리 안에 없는 메뉴 이름을 찾을 수 없는지 확인하는 테스트 추가

--- src/test/java/menu/domain/CategoryTest.java
+++ src/test/java/menu/domain/CategoryTest.java
@@ -23,4 +23,19 @@ public class CategoryTest {
         // then
         assertEquals(actual, expected, "[ERROR] 카테고리 안에 메뉴가 있는데 없다고 나옵니다.");
     }
+
+    @DisplayName("메뉴 이름을 찾을 수 없는지 확인하다.")
+    @Test
+    void checkFalseFindByName() {
+        // given
+        String value = "김치찌개";
+        Category category = new Category("일식", List.of("규동", "우동", "미소시루", "스시"));
+        boolean expected = false;
+
+        // when
+        boolean actual  = category.findByName(value);
+
+        // then
+        assertEquals(actual, expected, "[ERROR] 카테고리 안에 메뉴가 없는데 있다고 나옵니다.");
+    }
 }
