021de257fd90222650da6c3dffafa7ba0260bf05
Dabeen Jeong <hello70825@gmail.com>
2022-12-17 13:55:48 +0900

    feat: 카테고리 객체를 추가

---
+++ src/main/java/menu/domain/Category.java
@@ -0,0 +1,31 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+    private static String name;
+    private static List<String> foods = new ArrayList<>();
+
+    public void Category(String name, List<String> foods) {
+        this.name = name;
+        this.foods = foods;
+    }
+
+    public static void addFood(String name) {
+        foods.add(name);
+    }
+
+    public static Boolean findByName(String name) {
+        int result = (int) foods.stream()
+                .filter(food -> food.equals(name))
+                .count();
+
+        if (result != 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
