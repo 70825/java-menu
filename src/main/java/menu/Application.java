04f387453eb10f7e584cdf026509facdc57c9baa
Dabeen Jeong <hello70825@gmail.com>
2022-12-17 13:51:13 +0900

    feat: 메인에 컨트롤러 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,11 @@
 package menu;
 
+import menu.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        MainController mainController = new MainController();
+        mainController.start();
     }
 }
