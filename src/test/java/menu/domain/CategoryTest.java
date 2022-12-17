52335a7aa7899ebaf051bfc1a4ef38a2d9fb86e7
Dabeen Jeong <hello70825@gmail.com>
2022-12-17 16:53:51 +0900

    test: 카테고리 안에 존재하는 메뉴 이름을 찾을 수 있는지 확인하는 테스트 추가

---
+++ src/test/java/menu/domain/CategoryTest.java
@@ -0,0 +1,26 @@
+package menu.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CategoryTest {
+
+    @DisplayName("메뉴 이름을 찾을 수 있는지 확인하다.")
+    @Test
+    void checkTrueFindByName() {
+        // given
+        String value = "우동";
+        Category category = new Category("일식", List.of("규동", "우동", "미소시루", "스시"));
+        boolean expected = true;
+
+        // when
+        boolean actual  = category.findByName(value);
+
+        // then
+        assertEquals(actual, expected, "[ERROR] 카테고리 안에 메뉴가 있는데 없다고 나옵니다.");
+    }
+}
