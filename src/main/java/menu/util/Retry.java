469d800398862caaae0197cb1a795a107bd56cbc
JAY-Winter <rubat0@naver.com>
2023-11-24 19:05:23 +0900

    feat: 매개변수 입력값 추가 및 상태에 따른 재실행

--- src/main/java/menu/util/Retry.java
+++ src/main/java/menu/util/Retry.java
@@ -1,5 +1,6 @@
 package menu.util;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class Retry {
@@ -10,6 +11,17 @@ public class Retry {
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
             return retryOnException(operation);
+        } catch (IllegalStateException exception) {
+            return retryOnException(operation);
+        }
+    }
+
+    public static <T, R> R retryOnExceptionWithParam(Function<T, R> operation, T param) {
+        try {
+            return operation.apply(param);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return retryOnExceptionWithParam(operation, param);
         }
     }
 }
