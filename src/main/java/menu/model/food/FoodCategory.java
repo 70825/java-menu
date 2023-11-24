0f169458cf1eabdfce830bf0690598a4eb8d5900
JAY-Winter <rubat0@naver.com>
2023-11-24 19:04:59 +0900

    refactor: 카테고리

--- src/main/java/menu/model/food/FoodCategory.java
+++ src/main/java/menu/model/food/FoodCategory.java
@@ -1,5 +1,20 @@
 package menu.model.food;
 
 public enum FoodCategory {
-    일식, 한식, 중식, 아시안, 양식
+    일식("일식", 1), 한식("한식", 2), 중식("중식", 3), 아시안("아시안", 4), 양식("양식",
+        5),없음("없음",0);
+
+    private final String name;
+
+    private final int number;
+
+    FoodCategory(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public int getNumber() {
+
+        return this.number;
+    }
 }
