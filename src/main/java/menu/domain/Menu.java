5f3bf1620eccc20bfe4375d2dab5adce2eb227cd
jinhyeon <tiemo0708@naver.com>
2023-12-05 08:19:05 +0900

    feat: 메뉴와메뉴 카테고리 클래스 생성

---
+++ src/main/java/menu/domain/Menu.java
@@ -0,0 +1,5 @@
+package menu.domain;
+
+public class Menu {
+
+}
---
+++ src/main/java/menu/domain/MenuCategory.java
@@ -0,0 +1,9 @@
+package menu.domain;
+
+public enum MenuCategory {
+    JAPANESE,
+    KOREAN,
+    CHINESE,
+    ASIAN,
+    WESTERN;
+}
