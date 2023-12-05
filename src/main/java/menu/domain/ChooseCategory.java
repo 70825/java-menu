b8bc01565ac394cf3a8a9ffdf9e64f460ce4dace
jinhyeon <tiemo0708@naver.com>
2023-12-05 10:25:53 +0900

    fix: 매핑을 enumMap 에서 HashMap으로 변경

--- src/main/java/menu/domain/ChooseCategory.java
+++ src/main/java/menu/domain/ChooseCategory.java
@@ -1,18 +1,19 @@
 package menu.domain;
 
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 
 import static menu.domain.MenuCategory.*;
 
 public class ChooseCategory {
-    final Map<MenuCategory, Integer> map = new EnumMap<>(MenuCategory.class);
+    final Map<Integer, MenuCategory> map = new HashMap<>();
     public ChooseCategory(){
-        map.put(JAPANESE, 1);
-        map.put(KOREAN, 2);
-        map.put(CHINESE, 3 );
-        map.put(ASIAN, 4);
-        map.put(WESTERN, 5);
+        map.put(1, JAPANESE);
+        map.put(2, KOREAN);
+        map.put(3, CHINESE );
+        map.put(4, ASIAN);
+        map.put(5, WESTERN);
     }
     public void choose() {
 
