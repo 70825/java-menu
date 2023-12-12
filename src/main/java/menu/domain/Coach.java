33784d3055a7d7379cd8f77063322d5c4834bed6
taehoon <xogns1514@gmail.com>
2023-12-12 15:20:32 +0900

    feat(Coach): 코치가 못 먹는 메뉴 예외처리
    - 예외처리: 못 먹는 메뉴가 3개 이상일 경우

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,18 +1,32 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static menu.error.ErrorMessage.*;
 
 public class Coach {
 
     private String name;
+    private List<Menu> dislikeMenu;
 
     public Coach(String name) {
         validateCoach(name);
         this.name = name;
+        dislikeMenu = new ArrayList<>();
+    }
+
+    public void setDislikeMenu(List<Menu> dislikeMenu) {
+        validateDislikeMenu(dislikeMenu);
+        this.dislikeMenu = dislikeMenu;
     }
 
     private void validateCoach(String name) {
         checkName(name);
+    }
+
+    private void validateDislikeMenu(List<Menu> dislikeMenu) {
+        checkDislikeMenuCount(dislikeMenu);
     }
 
     private void checkName(String name) {
@@ -21,7 +35,17 @@ public class Coach {
         }
     }
 
+    private void checkDislikeMenuCount(List<Menu> dislikeMenu) {
+        if (!isValidDislikeMenuCount(dislikeMenu)) {
+            throw new IllegalArgumentException(INVALID_DISLIKE_MENU_COUNT_ERROR.getMessage());
+        }
+    }
+
     private boolean isNameValid(String name) {
         return 2 <= name.length() && name.length() <= 4;
+    }
+
+    private boolean isValidDislikeMenuCount(List<Menu> dislikeMenu) {
+        return 0 <= dislikeMenu.size() && dislikeMenu.size() <= 2;
     }
 }
--- src/main/java/menu/error/ErrorMessage.java
+++ src/main/java/menu/error/ErrorMessage.java
@@ -2,7 +2,8 @@ package menu.error;
 
 public enum ErrorMessage {
     INVALID_COACH_NAME_ERROR("코치 이름은 2글자 이상 4글자 이하입니다."),
-    INVALID_COACH_COUNT_ERROR("코치는 2명 이상 5명 이하로 입력해야 합니다.");
+    INVALID_COACH_COUNT_ERROR("코치는 2명 이상 5명 이하로 입력해야 합니다."),
+    INVALID_DISLIKE_MENU_COUNT_ERROR("먹지 못하는 메뉴는 0개 이상 2개 이하입니다.");
 
 
     private final String message;
